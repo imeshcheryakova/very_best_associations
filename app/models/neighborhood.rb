80737b26cf8fb9069bcf0d652cb7dacc91004d38
imeshcheryakova <imeshcher@chicagobooth.edu>
2015-08-14 22:48:53 -0500

    without forms

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -3,4 +3,5 @@ class Neighborhood < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => { :scope => :city }
   validates :city, :presence => true
   has_many :venues
+  has_many :favorites, :through => :venues
 end
