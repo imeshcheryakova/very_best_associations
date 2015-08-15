3cb7a97a73e0661fa8e281528aab9bdd065ebb1c
imeshcheryakova <imeshcher@chicagobooth.edu>
2015-08-15 08:24:52 -0500

    Complete

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,7 +1,7 @@
 class Venue < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => { :scope => :address }
   validates :address, :presence => true
-  validates :address, :format => {with: /\A[0-9]+ [a-zA-Z0-9\.\- ]+ [0-9]{5,5}\z/}
+  validates :address, :format => {with: /\A[0-9]+ [a-zA-Z0-9\.\- ,]+ [0-9]{5,5}\z/, message: "Example 345 E.Wacker,1311,Chicago,IL 60601"}
   validates :neighborhood_id, :presence => true
 
   belongs_to :neighborhood
